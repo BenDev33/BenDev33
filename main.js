c1d3fe5e7ee24ca14c52f4bec831455095786b17
Ben D <macuser@Macs-MacBook-Pro.local>
2019-06-18 13:44:07 -0400

    added surf shop project

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-window.sr = ScrollReveal();
-sr.reveal(".mountain", {
-    duration: 3000,
-    origin: 'top',
-    distance: '500px'
-});
-sr.reveal(".aboutme", {
-    duration: 3000,
-    origin: 'left',
-    distance: '100px',
-    viewfactor: 0.2
-});
-sr.reveal(".col-4", {
-    duration: 3000,
-    origin: 'right',
-    distance: '100px',
-    viewfactor: 0.2
-});
+// window.sr = ScrollReveal();
+// sr.reveal(".mountain", {
+//     duration: 3000,
+//     origin: 'top',
+//     distance: '500px'
+// });
+// sr.reveal(".aboutme", {
+//     duration: 3000,
+//     origin: 'left',
+//     distance: '100px',
+//     viewfactor: 0.2
+// });
+// sr.reveal(".col-4", {
+//     duration: 3000,
+//     origin: 'right',
+//     distance: '100px',
+//     viewfactor: 0.2
+// });
